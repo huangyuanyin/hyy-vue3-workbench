dafc80def7ece8d4c38dfabb8aa08a21918f414d
黄原寅 <3443718488@qq.com>
2023-01-04 10:33:08 +0800

    fix 跳转栏

--- src/components/Drawer.vue
+++ src/components/Drawer.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="drawer-wrapper">
-    <el-drawer v-model="isDrawer" :direction="direction" :append-to-body="false" :z-index="-1" :with-header="false"
-      size="60%">
+    <el-drawer v-model="isDrawer" :direction="direction" :append-to-body="false" :z-index="-1" :with-header="false" size="60%">
       <el-row>
         <el-col :span="4">
           <el-menu default-active="prodandserver" @select="handleSelect">
@@ -9,7 +8,8 @@
               <el-icon>
                 <Grid />
               </el-icon>
-              <template #title>产品与服务
+              <template #title
+                >产品与服务
                 <el-icon style="margin-left: 40px">
                   <ArrowRight />
                 </el-icon>
@@ -114,127 +114,127 @@ const props = defineProps({
     type: Boolean,
     default: false,
   },
-})
-const emit = defineEmits(['changeDrawer'])
+});
+const emit = defineEmits(["changeDrawer"]);
 
 const router = useRouter();
 const store = useAppStore();
-const filter = ref('') // 搜索
-const height = window.innerHeight - 52 + 'px'// 高度
+const filter = ref(""); // 搜索
+const height = window.innerHeight - 52 + "px"; // 高度
 // 测试模块
 const testData = ref([
   {
-    value: 'netpts',
-    label: '性能测试'
-  }, {
-    value: 'apitest',
-    label: '接口测试'
-  }, {
-    value: 'Agile',
-    label: '敏捷测试'
-  }, {
-    value: 'netapv',
-    label: 'APV自动化'
-  }, {
-    value: 'netpoc',
-    label: 'POC自动化'
-  }
-])
+    value: "netpts",
+    label: "性能测试",
+  },
+  {
+    value: "apitest",
+    label: "接口测试",
+  },
+  {
+    value: "Agile",
+    label: "敏捷测试",
+  },
+  {
+    value: "netapv",
+    label: "APV自动化",
+  },
+  {
+    value: "netpoc",
+    label: "POC自动化",
+  },
+]);
 // 监控与运维服务
 const monitorData = ref([
   {
-    value: 'netcmt',
-    label: '信安云网管'
-  }, {
-    value: 'netmonitor',
-    label: '监控服务'
-  }, {
-    value: 'netcfg',
-    label: '混沌演练平台'
-  }
-])
+    value: "netcmt",
+    label: "信安云网管",
+  },
+  {
+    value: "netmonitor",
+    label: "监控服务",
+  },
+  {
+    value: "netcfg",
+    label: "混沌演练平台",
+  },
+]);
 const dataServices = ref([
   {
-    value: 'netforum',
-    label: '信安知识库'
-  }, {
-    value: 'netevaluate',
-    label: '度量分析'
-  }
-])
+    value: "netforum",
+    label: "信安知识库",
+  },
+  {
+    value: "netevaluate",
+    label: "度量分析",
+  },
+]);
 // 容器与虚拟机服务
 const containerData = ref([
   {
-    value: 'container',
-    label: '容器镜像服务'
-  }, {
-    value: 'container',
-    label: '容器服务'
-  }, {
-    value: 'vm',
-    label: '虚拟机服务'
-  }
-])
+    value: "container",
+    label: "容器镜像服务",
+  },
+  {
+    value: "container",
+    label: "容器服务",
+  },
+  {
+    value: "vm",
+    label: "虚拟机服务",
+  },
+]);
 // 安全服务
 const securityData = ref([
   {
-    value: 'netpt',
-    label: '渗透测试服务'
-  }, {
-    value: 'netvs',
-    label: '漏洞扫描'
-  }, {
-    value: 'netdos',
-    label: 'DDos高防'
-  }
-])
+    value: "netpt",
+    label: "渗透测试服务",
+  },
+  {
+    value: "netvs",
+    label: "漏洞扫描",
+  },
+  {
+    value: "netdos",
+    label: "DDos高防",
+  },
+]);
 // 测试工具
 const toolData = ref([
   {
-    value: 'cert',
-    label: '证书服务'
-  }
-])
+    value: "cert",
+    label: "证书服务",
+  },
+]);
 // 跳转
 const goTo = (value: any) => {
   ElMessage({
-    type: 'warning',
-    message: '暂不支持！！！',
-  })
-}
+    type: "warning",
+    message: "暂不支持！！！",
+  });
+};
 // 侧边栏响应
 const handleSelect = (value: any) => {
-  let url = ''
-  let console_url = sessionStorage.getItem("CONSOLE_URL")
-  localStorage.setItem('jwtToken', sessionStorage.getItem("token"))
-  url = console_url + '/' + value
-  if (value === 'prodandserver') {
-    return
-  }
-  if (value == 'netpts') {
-    url += '/#/center/mine'
-  }
-  if (value === 'apitest') {
-    url = ''
-    url = console_url + '/#/center/mine'
-  }
-  window.location.href = url
-}
+  let url = "";
+  let console_url = sessionStorage.getItem("CONSOLE_URL");
+  localStorage.setItem("jwtToken", sessionStorage.getItem("token"));
+  url = console_url + "/" + value;
+  window.location.href = url;
+};
 const direction = ref("ltr");
 const isDrawer = computed({
   get() {
-    return props.drawer
+    return props.drawer;
   },
   set(value) {
-    emit("changeDrawer", value)
+    emit("changeDrawer", value);
     return value;
-  }
-})
+  },
+});
 
 const handleClose = () => {
   isDrawer.value = false;
-}
-
+};
 </script>
 
 <style lang="scss" scoped>
@@ -260,7 +260,7 @@ const handleClose = () => {
 
   .span-title {
     font-weight: 800;
-    font-family: '宋体';
+    font-family: "宋体";
   }
 
   .button {
--- src/config/api.js
+++ src/config/api.js
@@ -1,6 +1,6 @@
 // 导出 对应的不同接口的URL地址
 const configUrl = {
-  Base_Login_URL: "http://10.4.150.27:8013", // 登录 接口地址
+  Base_Login_URL: "http://10.4.150.55:8013", // 登录 接口地址
   Base_Xterm_URL: "http://10.20.86.27:1182", // xterm 接口地址
 };
 export default configUrl;
