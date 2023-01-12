52f6b4a844ff62e90d49dbb01d38394f9dbd6911
黄原寅 <3443718488@qq.com>
2023-01-12 16:08:06 +0800

    登录接口

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,68 +1,60 @@
-import axios from "axios";
-import { ElMessage } from "element-plus";
-// import { useCookies } from '@vueuse/integrations/useCookies'
-import { getToken } from "./auth";
-import baseUrl from "@/config/api";
+import axios from 'axios'
+import { ElMessage } from 'element-plus'
+import { getToken } from './auth'
 
 const service = axios.create({
-  // baseURL: '/api',
-  timeout: 12000,
-});
+  timeout: 12000
+})
 
 // 添加请求拦截器
 service.interceptors.request.use(
   function (config) {
     // 在发送请求之前做些什么
     // 往header头中自动添加token
-    const hastoken = getToken();
-    if (
-      hastoken &&
-      config.url !== "/forum/login/" &&
-      config.url !== "/datas/datas/" &&
-      config.url !== "/WEBt/terminals/"
-    ) {
-      config.headers["token"] = hastoken;
+    const hastoken = getToken()
+    if (hastoken && config.url !== '/forum/login/' && config.url !== '/datas/datas/' && config.url !== '/WEBt/terminals/') {
+      config.headers['token'] = hastoken
     }
     switch (config.urlType) {
-      case "Xterm":
-        config.url = baseUrl.Base_Xterm_URL + config.url;
-        break;
-      case "Login":
-        config.url = baseUrl.Base_Login_URL + config.url;
-        break;
+      case 'Xterm':
+        config.url = import.meta.env.VITE_BASE_LOGIN_URL + config.url
+        break
+      case 'Login':
+        config.url = import.meta.env.VITE_BASE_LOGIN_URL + config.url
+        break
     }
     // 请求拦截进来 显示loading效果
-    return config;
+    return config
   },
   function (error) {
     // 对请求错误做些什么
-    return Promise.reject(error);
+    return Promise.reject(error)
   }
-);
+)
 
 // 添加响应拦截器
 service.interceptors.response.use(
   function (response) {
     // 对响应数据做点什么
-    if (response.data.code == "1001") {
+    if (response.data.code == '1001') {
       ElMessage({
-        message: response.data?.msg || "请求失败",
-        type: "error",
-        duration: 1000,
-      });
-      return Promise.reject(response);
+        message: response.data?.msg || '请求失败',
+        type: 'error',
+        duration: 1000
+      })
+      return Promise.reject(response)
     }
-    return response.data;
+    return response.data
   },
   function (error) {
     // 对响应错误做点什么
     ElMessage({
-      message: error.data?.msg || "请求失败",
-      type: "error",
-      duration: 1000,
-    });
-    return Promise.reject(error);
+      message: error.data?.msg || '请求失败',
+      type: 'error',
+      duration: 1000
+    })
+    return Promise.reject(error)
   }
-);
+)
 
-export default service;
+export default service
